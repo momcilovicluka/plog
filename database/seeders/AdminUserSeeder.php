0c83bf493eb31f0b206a06d121bd729c27f54953
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-25 22:46:27 +0200

    feat(seeder): add admin seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (User::where('email', 'admin@example.com')->exists()) {
+            return;
+        }
+
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+    }
+}
