b555f20c4e4b1a8fd35d048e5f4005b66b5d7822
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-25 21:26:06 +0200

    feat(seeder): add guest user to seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -15,9 +17,14 @@ class UserSeeder extends Seeder
     {
         User::factory()->count(10)->create(); // Creates 10 random users
 
-        /* User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]); */
+        User::factory()->create([
+            'id' => -1,
+            'name' => 'Guest',
+            'email' => 'guest@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'role' => 'user',
+        ]);
     }
 }
