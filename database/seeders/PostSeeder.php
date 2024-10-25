174d0a366a73371ea8a76d2f2808cdba0e3b331d
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-25 10:11:40 +0200

    feat(db): add post seeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    public function run()
+    {
+        // Get all users
+        $users = User::all();
+
+        // Create 3-5 posts for each user
+        foreach ($users as $user) {
+            Post::factory()->forUser($user)->count(2)->create();
+        }
+    }
+}
+
