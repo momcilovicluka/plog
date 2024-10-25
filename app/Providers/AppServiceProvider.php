2203550ae34ce72a611e73cd81859efbe5e3c1f6
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-25 21:24:04 +0200

    feat(provider): register policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,20 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Policies\PostPolicy;
+use App\Policies\CommentPolicy;
+use Illuminate\Contracts\Auth\Access\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
+    
+    protected $policies = [
+        Post::class => PostPolicy::class,
+        Comment::class => CommentPolicy::class,
+    ];
+
     /**
      * Register any application services.
      */
@@ -20,6 +31,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->registerPolicies();
         Vite::prefetch(concurrency: 3);
+    }
+
+    /**
+     * Register the application's policies.
+     */
+    public function registerPolicies(): void
+    {
+        $gate = $this->app->make(Gate::class);
+
+        foreach ($this->policies as $model => $policy) {
+            $gate->policy($model, $policy);
+        }
     }
 }
