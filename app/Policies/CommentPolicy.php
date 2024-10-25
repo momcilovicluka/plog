2f525ff0dacbedb4db5d26563f3c63b4dfc2dc77
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-25 21:23:30 +0200

    feat(policy): add comment policy

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CommentPolicy
+{
+    // Determine if the user can update the comment
+    public function update(User $user, Comment $comment)
+    {
+        return $user->id === $comment->user_id ? Response::allow() : Response::deny('You do not own this comment.');
+    }
+
+    // Determine if the user can delete the comment
+    public function delete(User $user, Comment $comment)
+    {
+        return $user->id === $comment->user_id || $user->id === $comment->post->user_id ? Response::allow() : Response::deny('You are not authorized to delete this comment.');
+    }
+
+    public function before(User $user, string $ability): bool|null
+{
+    if ($user->role === 'admin') {
+        return true;
+    }
+ 
+    return null;
+}
+}
