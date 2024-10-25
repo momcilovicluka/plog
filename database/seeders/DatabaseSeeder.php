01a6d31433da4b6faee984697c2e437db122ec7c
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-25 22:47:09 +0200

    feat(seeder): add admin seeder call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            AdminUserSeeder::class,
             PostSeeder::class,
             CommentSeeder::class,
         ]);
