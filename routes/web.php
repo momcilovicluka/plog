5f5539f9345f77ca5fb4e30a80e877c2603b0138
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-25 21:27:40 +0200

    feat(route): add user's posts route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Models\Post;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -46,5 +47,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/posts/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/users/{user}/posts', [UserController::class, 'userPosts'])->name('users.posts');
 
 require __DIR__ . '/auth.php';
