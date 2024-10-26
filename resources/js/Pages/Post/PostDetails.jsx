005d2261da52196c540f96fd8ad88f25f9db9ca5
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-26 20:45:28 +0200

    feat(page): add timestamp and style to post details

--- resources/js/Pages/Post/PostDetails.jsx
+++ resources/js/Pages/Post/PostDetails.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from '@inertiajs/react';
+import moment from 'moment';
 
 export default function PostDetails({ post }) {
     return (
-        <div>
+        <div className='bg-gray-800 p-6 rounded-lg shadow-lg m-2'>
             <h1 className="text-3xl font-bold mb-4 text-gray-200">{post.title}</h1>
             <h3 className="font-bold mb-4 text-blue-300">
                 <Link
@@ -12,6 +13,7 @@ export default function PostDetails({ post }) {
                 >
                     {post.user.name}
                 </Link>
+                <p className='text-purple-700'>on {moment(post.created_at).format('MMMM Do YYYY, h:mm:ss a')} </p>
             </h3>
             <p className="text-gray-300 mb-6">{post.content}</p>
         </div>
