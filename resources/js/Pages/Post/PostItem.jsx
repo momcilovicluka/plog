2b3b0da7ce867088e91286d0ad76ab47669e7cfc
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-26 19:38:48 +0200

    feat(page): change post item style

--- resources/js/Pages/Post/PostItem.jsx
+++ resources/js/Pages/Post/PostItem.jsx
@@ -3,9 +3,9 @@ import { Link } from '@inertiajs/react';
 
 export default function PostItem({ post }) {
     return (
-        <li className="bg-gray-800 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 list-none">
+        <li className="post-item bg-gray-800 p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 list-none">
             <Link href={`/posts/${post.id}`}>
-                <h2 className="text-xl font-semibold text-blue-400 hover:underline">
+                <h2 className="text-xl font-semibold text-blue-400">
                     {post.title}
                 </h2>
                 <p className="mt-2 text-gray-300">
