5b9d1cfbdf99db17ecab4ddf08383546cdb0162a
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-27 19:25:40 +0100

    :lock:feat(policy): remove admin's ability to edit posts

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -11,24 +11,12 @@ class PostPolicy
     // Determine if the user can update the post
     public function update(User $user, Post $post)
     {
-        return ($user->id === $post->user_id || $user->role === 'admin') ? Response::allow() : Response::deny('You do not own this post.');
+        return ($user->id === $post->user_id) ? Response::allow() : Response::deny('You do not own this post.');
     }
 
     // Determine if the user can delete the post
     public function delete(User $user, Post $post)
     {
         return ($user->id === $post->user_id || $user->role === 'admin') ? Response::allow() : Response::deny('You do not own this post.');
-    }
-
-    /**
-     * Perform pre-authorization checks.
-     */
-    public function before(User $user, string $ability): ?bool
-    {
-        if ($user->role === 'admin') {
-            return true;
-        }
-
-        return null;
     }
 }
