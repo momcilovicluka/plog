d689f2b46b88086373f6dc67879e7c4d4528cd45
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-27 19:45:33 +0100

    :bulb:docs(http): remove self evident comments from user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,8 @@ class UserController extends Controller
 {
     public function userPosts(User $user)
     {
-        // Load the user's posts with pagination, eager-loading relationships if needed
         $posts = $user->posts()->latest()->paginate(10);
 
-        // Return the view with Inertia
         return Inertia::render('UserPosts', [
             'user' => $user,
             'posts' => $posts,
