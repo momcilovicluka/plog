0e224cb5a99a266c265f5a1a027e993026c19c9f
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-27 19:45:44 +0100

    :bulb:docs(http): remove self evident comments from comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -11,12 +11,9 @@ class CommentController extends Controller
 {
     public function destroy($id)
     {
-        // Retrieve the comment by ID
         $comment = Comment::findOrFail($id);
 
-        // Check if the authenticated user is the owner of the comment or the owner of the post
         if (Auth::user()->id === $comment->user_id || Auth::user()->id === $comment->post->user_id) {
-            // Delete the comment
             $comment->delete();
 
             return redirect()->back()->with('success', 'Comment deleted successfully.');
@@ -27,24 +24,19 @@ class CommentController extends Controller
 
     public function store(Request $request, $postId)
     {
-        // Validate the incoming request
         $request->validate([
             'comment' => 'required|string|max:255',
         ]);
 
-        // Find the post by ID
         $post = Post::findOrFail($postId);
 
-        // Create a new comment instance
         $comment = new Comment;
         $comment->comment = $request->input('comment');
         $comment->post_id = $post->id;
-        $comment->user_id = Auth::check() ? Auth::id() : -1; // Set user_id for authenticated users
+        $comment->user_id = Auth::check() ? Auth::id() : -1;
 
-        // Save the comment
         $comment->save();
 
-        // Redirect back to the post with a success message
         return redirect()->route('posts.show', $postId)->with('success', 'Comment added successfully!');
     }
 }
