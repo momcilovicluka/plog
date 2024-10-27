fea664a2eb8548bc0c0bd283bad00f23af0f24e4
Luka Momčilović <l.momcilovic61@gmail.com>
2024-10-27 19:24:04 +0100

    :bulb:docs(policy): remove self evident comments

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -8,13 +8,11 @@ use Illuminate\Auth\Access\Response;
 
 class CommentPolicy
 {
-    // Determine if the user can update the comment
     public function update(User $user, Comment $comment)
     {
         return $user->id === $comment->user_id ? Response::allow() : Response::deny('You do not own this comment.');
     }
 
-    // Determine if the user can delete the comment
     public function delete(User $user, Comment $comment)
     {
         return $user->id === $comment->user_id || $user->id === $comment->post->user_id || $user->role === 'admin' ? Response::allow() : Response::deny('You are not authorized to delete this comment.');
